245a4b105d24de208dc49c17a560caabbf24f0b1
Baophuc Pham <baophucpham74@gmail.com>
2021-04-24 16:59:25 +0700

    commit

--- src/app/modules/list-ppc/list-ppc.component.ts
+++ src/app/modules/list-ppc/list-ppc.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem, PrimeNGConfig } from 'primeng/api';
+import { Product } from './product';
+import { ProductService } from './productservice';
+import { SortEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-list-ppc',
   templateUrl: './list-ppc.component.html',
   styleUrls: ['./list-ppc.component.scss'],
 })
-
-
-
-
 export class ListPPCComponent implements OnInit {
-  constructor() {}
+  products1: Product[] = [];
+  products2: Product[] = [];
+  products3: Product[] = [];
 
-  ngOnInit(): void {
+  constructor(private productService: ProductService) {}
 
+  ngOnInit() {
+    // this.productService
+    //   .getProductsSmall()
+    //   .then((data) => {
+    //     this.products1 = data;
+    //     console.log("bfshdbfsdfshdjhk", this.products1);
+    //   }
+    //   );
+    // this.productService
+    //   .getProductsSmall()
+    //   .then((data) => (this.products2 = data));
+    // this.productService
+    //   .getProductsSmall()
+    //   .then((data) => (this.products3 = data));
+
+    this.productService.mockApi().subscribe((res) => {
+      console.log(res);
+    });
   }
 }
